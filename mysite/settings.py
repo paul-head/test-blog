3f796ce924c0ef640df7a6dde7f07aac4d4fccd8
Pavel Golovanov <publichead@yandex.ru>
2019-10-14 00:47:45 +0300

    add site map. editted main url and add sitemap

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SITE_ID = 1
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
