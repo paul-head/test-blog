211d03420220789f93da640e169afbf1c8aff081
Pavel Golovanov <publichead@yandex.ru>
2019-10-16 00:45:48 +0300

    edit post_search

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.db.models import Count
 from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -123,9 +123,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html', {
         'form': form,
